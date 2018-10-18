40eb72c596ec7a9535debb632f3389a80b060b12
Njuguna <njuguna2@illinois.edu>
2018-10-18 09:36:49 -0500

    more comments

--- Lab8 Script.R
+++ Lab8 Script.R
@@ -1,6 +1,6 @@
 #this code calcuates growing degree days
 growingDegreesTimesTen <- function(x){
-  a <- x-100L
+  a <- x-100L#minus 100 to remove 10 degrees (multipled by 10 to convert decimals to integers )
   b <- a < 0L
   c <- a > 200L
   a[b] <- 0L
